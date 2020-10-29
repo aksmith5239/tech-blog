70ce9e0f21b42d54cfe00bf05fe7e1519d328ad8
aksmith5239 <aklobby@gmail.com>
2020-10-29 16:12:03 -0600

    template styles started

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const sequelize = require('./config/connection');
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// app.use(routes);
 
 
 
