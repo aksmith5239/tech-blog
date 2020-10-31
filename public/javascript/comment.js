0d585aa51795edb315eb2e9ed48960934e324aa0
aksmith5239 <aklobby@gmail.com>
2020-10-31 15:58:42 -0600

    single post page completed

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -3,9 +3,11 @@ async function commentFormHandler(event) {
 
     const comment_text = document.querySelector('textarea[name="comment-body').value.trim();
     const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/') -1
+        window.location.toString().split('/').length -1
     ];
+
     if(comment_text) {
+       
         const response = await fetch('/api/comments', {
             method: 'POST',
             body: JSON.stringify({
