a055756772b120bc93e7b5569beabd94e7333241
aksmith5239 <aklobby@gmail.com>
2020-11-01 14:23:35 -0700

    server update

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.use(routes);
 
 
 
-sequelize.sync({force: false}).then(() => {
+sequelize.sync({force: true}).then(() => {
     app.listen(PORT, ()=> console.log(`Now Listening on port ${PORT}!`));
 })
