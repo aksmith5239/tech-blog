64b96419408e1b068c88edb405ddb5510dd29bf5
aksmith5239 <aklobby@gmail.com>
2020-11-01 15:21:48 -0700

    session updates

--- server.js
+++ server.js
@@ -6,33 +6,19 @@ const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const sequelize = require('./config/connection');
 const session = require('express-session');
-// const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-// app.use(express.cookieParser());
-
-
-// const sess = cookieSession({
-//     secret: process.env.SECRET,
-//     resave: false,
-//     cookie: {
-//         secureProxy: true,
-//         httpOnly: true,
-//         domain: "https://my-tech-blog-2020.herokuapp.com/",
-//         expires: expiryDate
-//     }
-// })
 
 const sess = {
     secret: 'secretsauce',
-    cookie: {},
+    cookie: {maxAge: 60000},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
         db: sequelize
     })
 };
-
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -42,12 +28,13 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(session(sess));
 
 app.use(routes);
 
 
 
-sequelize.sync({force: true}).then(() => {
+sequelize.sync({force: false}).then(() => {
     app.listen(PORT, ()=> console.log(`Now Listening on port ${PORT}!`));
 })
